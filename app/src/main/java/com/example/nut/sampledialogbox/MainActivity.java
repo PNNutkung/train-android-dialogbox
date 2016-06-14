541c3e487e6db6a767c0f1370993e6c051c37c8c
PNNutkung <pipatpol.t@ku.th>
2016-06-14 19:15:06 +0700

    Finish

--- app/src/main/java/com/example/nut/sampledialogbox/MainActivity.java
+++ app/src/main/java/com/example/nut/sampledialogbox/MainActivity.java
@@ -1,7 +1,13 @@
 package com.example.nut.sampledialogbox;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.DialogInterface;
 import android.os.Bundle;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick(R.id.main_open_dialog_btn)
+    public void onClick() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+        builder.setMessage("ว่างเหรอครับ?");
+        builder.setPositiveButton("ว่าง", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getApplicationContext(), "ทำงานสิครับ", Toast.LENGTH_LONG).show();
+            }
+        });
+        builder.setNegativeButton("ไม่ว่าง", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
+        builder.show();
     }
 }
